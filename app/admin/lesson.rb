4863ebbe6729bfea73d97c2ba6c66d26582e2243
ikepon <tatsuyanoheya@ikedas-MacBook-Air.local>
2014-05-30 11:55:23 +0900

    heroku_test

--- app/admin/lesson.rb
+++ app/admin/lesson.rb
@@ -1,6 +1,44 @@
 ActiveAdmin.register Lesson do
 
-  
+  permit_params :title, :summary, :movie, :time, :grade, :category_name, :category_summary, :unit_name, :unit_summary, :unit_item_name, :unit_item_summary, :explanation,
+    :exercise, :exercise_answer, :point, :number, :exe_pdf
+
+  index do
+    column :id
+    column :grade
+    column :unit_name
+    column :unit_item_name
+    column :title
+    column :summary
+    column :number
+    column :category_name
+    column :created_at
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "Lesson" do
+      f.input :title
+      f.input :summary
+      f.input :movie
+      f.input :time, :as => :datetime_picker
+      f.input :grade
+      f.input :category_name
+      f.input :category_summary
+      f.input :unit_name
+      f.input :unit_summary
+      f.input :unit_item_name
+      f.input :unit_item_summary
+      f.input :explanation
+      f.input :exercise
+      f.input :exercise_answer
+      f.input :point
+      f.input :number
+    end
+    f.actions
+  end
+
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -13,5 +51,5 @@ ActiveAdmin.register Lesson do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
 end
